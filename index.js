3f7937ded04cbcd1c6f41f94e3432b90129a4691
Soheyl farzane Roodpishi <54146841+soheylfarzane@users.noreply.github.com>
2023-12-22 12:28:33 +0330

    Update index.js
    تغییر ساختار و بازنویسی امکان استفاده برای تمام API های فیلتر شده

--- index.js
+++ index.js
@@ -1,59 +1,27 @@
-const URL_PATH_REGEX = /^\/bot(?<bot_token>[^/]+)\/(?<api_method>[a-z]+)/i;
-async function handleTelegramRequest(request) {
-	const url = new URL(request.url);
-
-	// آدرس URL را به API تلگرام تغییر می‌دهد.
-	url.hostname = 'api.telegram.org';
-
-	// یک درخواست جدید با آدرس تغییر یافته ایجاد می‌کند.
-	const newRequest = new Request(url.toString(), request);
-
-	// پاسخ از API را دریافت می‌کند.
-	const response = await fetch(newRequest);
-
-	return response;
-}
-
-function handleRootRequest() {
-	const result = 'همه چیز درسته فقط به جای api تلگرام آدرس ورکر رو بزار و تمام.';
-
-	return new Response(JSON.stringify({ ok: true, result }), {
-		status: 200,
-		statusText: result,
-		headers: {
-			'content-type': 'application/json',
-		},
-	});
-}
-
-
-async function handle404Request() {
-	const description = 'این مسیر غلطه یه سر به داکیومنت های تلگرام بزن';
-	const error_code = 404;
-
-	return new Response(JSON.stringify({ ok: false, error_code, description }), {
-		status: error_code,
-		statusText: description,
-		headers: {
-			'content-type': 'application/json',
-		},
-	});
-}
-
 async function handleRequest(request) {
-	const { pathname } = new URL(request.url);
-
-	if (URL_PATH_REGEX.test(pathname)) {
-		return await handleTelegramRequest(request);
+	const url = new URL(request.url);
+  
+	// Change the hostname to 'api.telegram.org'
+	url.hostname = 'api.telegram.org';
+  
+	// Create a new request with the modified URL
+	const newRequest = new Request(url.toString(), request);
+  
+	// Fetch and return the response from the new URL
+	try {
+	  const response = await fetch(newRequest);
+	  return response;
+	} catch (error) {
+	  // Handle errors, if any
+	  return new Response('Internal Server Error', {
+		status: 500,
+		headers: {
+		  'content-type': 'text/plain',
+		},
+	  });
 	}
-
-	if (pathname === '/') {
-		return handleRootRequest();
-	}
-
-	return handle404Request();
-}
-
-addEventListener('fetch', (event) => {
+  }
+  
+  addEventListener('fetch', (event) => {
 	event.respondWith(handleRequest(event.request));
-});
+  });
